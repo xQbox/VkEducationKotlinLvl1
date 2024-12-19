37ba4158335304195a9b38244a5fa769aac99fca
Aleksei <alekseirodinkov21@gmail.com>
2024-12-20 00:16:51 +0300

    Front Part 1

--- lab_02/app/src/main/java/com/example/myapplication/ItemListScreen.kt
+++ lab_02/app/src/main/java/com/example/myapplication/ItemListScreen.kt
@@ -1,22 +1,389 @@
 // ItemsListScreen.kt
 package com.example.myapp.com.example.myapplication
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.myapplication.Item
 import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
 import com.example.myapp.DataRepository
+import com.example.myapplication.R
 
 
+@Preview
+@Composable
+fun MainScreen() {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color(0xFF2C2C2C))
+    )
+    {
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top, // Элементы будут размещены сверху вниз
+            horizontalAlignment = Alignment.CenterHorizontally // Центрирование элементов по горизонтали
+        ) {
+            shadowLayout()
+            Header() // Заголовок ниже логотипа
+            FilterAndSearch()
+            AlbumSelectionScreen()
+        }
+    }
+}
+
+@Composable
+fun shadowLayout() {
+    Logo() // Логотип сверху
+}
+
+@Composable
+fun Logo() {
+//    val defaultSpaceWidthLogo = 55 && 45
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 2.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Spacer(modifier = Modifier.width(55.dp))
+        Icon(
+            painter = AssetImagePainter("1.png"), // Замените "logo.png" на имя файла с логотипом
+            contentDescription = "App Logo",
+            modifier = Modifier
+                .height(100.dp)
+                .width(100.dp)
+        )
+        // Название приложения
+        Text(
+            text = "VINIL-CLUB",
+            style = MaterialTheme.run {
+                typography.headlineLarge.copy(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 30.sp, // Увеличиваем размер шрифта
+                        color = Color.White
+                    )
+            },
+            modifier = Modifier
+                .padding(start = 8.dp)
+        )
+        Spacer(modifier = Modifier.width(35.dp))
+    }
+}
+
+@Composable
+fun Header() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly // Равномерное распределение кнопок
+    ) {
+        Button(
+            shape = RoundedCornerShape(8.dp),
+            onClick = { /* Навигация */ },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFFF6A00),
+                contentColor = Color.White
+            ),
+        ) { Text("Собрания") }
+
+        Button(
+            shape = RoundedCornerShape(8.dp),
+            onClick = { /* Навигация */ },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFFF6A00),
+                contentColor = Color.White
+            ),
+        ) { Text("Каталог") }
+
+        Button(
+            shape = RoundedCornerShape(8.dp),
+            onClick = { /* Навигация */ },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFFF6A00),
+                contentColor = Color.White
+            ),
+        ) { Text("Категории") }
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FilterAndSearch() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp), // Added padding to the Row
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        // Кнопка "Фильтр" with weight
+        Button(
+            onClick = { /* Фильтр */ },
+            modifier = Modifier
+                .height(48.dp)
+                .weight(0.3f), // Assign weight to the Button
+            shape = RoundedCornerShape(8.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Black,
+                contentColor = Color.White
+            ),
+            border = BorderStroke(2.dp, Color(0xFFFF6A00))
+        ) {
+            Text("Фильтр")
+            Icon(
+                imageVector = Icons.Default.ArrowDropDown,
+                contentDescription = "Dropdown",
+                modifier = Modifier.size(16.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.width(4.dp)) // Adjust spacer as needed
+
+        // Поле ввода с иконкой with weight
+        TextField(
+            value = "",
+            onValueChange = { /* Обработка ввода */ },
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "Search Icon",
+                    modifier = Modifier.size(24.dp),
+                    tint = Color.Black
+                )
+            },
+            placeholder = {
+                Text("Поиск пластинки", color = Color.Gray)
+            },
+            modifier = Modifier
+                .weight(0.7f) // Assign weight to the TextField
+                .height(52.dp),
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = Color.White,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            )
+        )
+    }
+}
+
+@Composable
+fun AlbumSelectionScreen() {
+    Column(
+        modifier = Modifier
+            .background(Color(0xFF2C2C2C))
+            .fillMaxSize()
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(), // Растянуть Row на всю ширину
+            horizontalArrangement = Arrangement.Center // Разместить содержимое по центру горизонтально
+        ) {
+            Text(
+                text = "Культовое",
+                color = Color.White,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(vertical = 16.dp), // Отступы сверху и снизу
+                textAlign = TextAlign.Center, // Центрирует текст внутри элемента Text
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+        )
+        {
+            AlbumRow(
+                albums = listOf(
+                    Album("AC/DC (ост \"Iron Man 2\")", "Блюз-рок, хард-рок", 5400, 152),
+                    Album("Royal Blood — \"How Did We Get So Dark?\"", "гаражный рок, альтернативный рок", 2200, 0)
+            )
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(), // Растянуть Row на всю ширину
+            horizontalArrangement = Arrangement.Center // Разместить содержимое по центру горизонтально
+        ) {
+            Text(
+                text = "Выбор редакции",
+                color = Color.White,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(vertical = 16.dp), // Отступы сверху и снизу
+                textAlign = TextAlign.Center, // Центрирует текст внутри элемента Text
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+        )
+        {
+            AlbumRow(
+                albums = listOf(
+                    Album("AC/DC (ост \"Iron Man 2\")", "Блюз-рок, хард-рок", 5400, 152),
+                    Album("Royal Blood — \"How Did We Get So Dark?\"", "гаражный рок, альтернативный рок", 2200, 0)
+                )
+            )
+        }
+
+    }
+}
+
+@Composable
+fun AlbumRow(albums: List<Album>) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        ProductBox()
+    }
+}
+
+@Composable
+fun ProductBox() {
+    val configuration = LocalConfiguration.current
+    val screenWidth = configuration.screenWidthDp.dp
+    val context = LocalContext.current
+
+    // Определяем размеры в зависимости от платформы
+    val boxSize = if (screenWidth < 600.dp) {
+        150.dp
+    } else {
+        200.dp
+    }
+
+    Column(
+        modifier = Modifier
+            .background(Color(0xFF333333))
+    ) {
+        Box(
+            modifier = Modifier
+                .size(boxSize)
+                .background(Color.White, shape = RoundedCornerShape(8.dp))
+        ) {
+            Image(
+                painter = AssetImagePainter("3.png"),
+                contentDescription = "Album Cover",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
+
+            IconButton(
+                onClick = {
+                    // Логика добавления в корзину
+                },
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .background(Color.Black, shape = RoundedCornerShape(50))
+            ) {
+                Icon(
+                    painter = AssetImagePainter("1.png"),
+                    contentDescription = "Add to Cart",
+                    tint = Color.White
+                )
+            }
+        }
+
+        Box(
+            modifier = Modifier.background(Color(0xFFFF6A00), shape = RoundedCornerShape(12.dp)),
+            contentAlignment = Alignment.CenterStart
+
+        ) {
+            Column {
+                Text(text = "AC/DC (ost 'Iron Man 2')", color = Color.White, fontSize = 16.sp)
+                Text(
+                    text = "Жанр: блоз-рок, хард-рок",
+                    color = Color.White.copy(alpha = 0.7f),
+                    fontSize = 12.sp
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = "5400 ₽", color = Color.White, fontSize = 14.sp)
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(
+                    text = "Товаров в наличии: 152",
+                    color = Color.White,
+                    fontSize = 12.sp
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun AlbumCard(album: Album) {
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .background(Color(0xFFFF6A00), shape = RoundedCornerShape(8.dp))
+            .padding(8.dp)
+            .width(150.dp)
+    ) {
+        // Mock image
+        Box(modifier = Modifier.height(100.dp).background(Color.Gray))
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = album.title, color = Color.White, fontSize = 12.sp, fontWeight = FontWeight.Bold)
+        Text(text = "Жанр: ${album.genre}", color = Color.White, fontSize = 10.sp)
+        Text(text = "${album.price}₽", color = Color.White, fontSize = 10.sp, fontWeight = FontWeight.Bold)
+        Text(text = "Товаров в наличии: ${album.stock}", color = Color.White, fontSize = 10.sp)
+    }
+}
+
+data class Album(val title: String, val genre: String, val price: Int, val stock: Int)
+
+//// ItemsListScreen.kt
+//package com.example.myapp.com.example.myapplication
+//
+//import androidx.compose.foundation.clickable
+//import androidx.compose.foundation.layout.*
+//import androidx.compose.foundation.lazy.*
+//import androidx.compose.material3.*
+//import androidx.compose.runtime.*
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.unit.dp
+//import androidx.navigation.NavController
+//import androidx.compose.ui.Alignment
+//import androidx.compose.ui.platform.LocalContext
+//import androidx.compose.ui.res.painterResource
+//import com.example.myapplication.Item
+//import coil.compose.AsyncImage
+//import com.example.myapp.DataRepository
+//
+//
 @Composable
 fun ItemsListScreen(navController: NavController) {
     val context = LocalContext.current
@@ -43,7 +410,7 @@ fun ItemsListScreen(navController: NavController) {
         }
     }
 }
-
+//// Функция описывает
 @Composable
 fun LoadingIndicator() {
     // Центрируем индикатор загрузки на экране
@@ -54,6 +421,7 @@ fun LoadingIndicator() {
     }
 }
 
+//
 @Composable
 fun ItemsList(items: List<Item>, navController: NavController) {
     // Отображаем список с элементами
@@ -67,7 +435,7 @@ fun ItemsList(items: List<Item>, navController: NavController) {
         }
     }
 }
-
+//
 
 @Composable
 fun ItemRow(item: Item, onClick: () -> Unit) {
@@ -91,3 +459,9 @@ fun ItemRow(item: Item, onClick: () -> Unit) {
     }
 }
 
+
+
+@Composable
+fun AssetImagePainter(imageFileName: String): Painter {
+    return rememberAsyncImagePainter("file:///android_asset/$imageFileName")
+}
--- lab_02/app/src/main/java/com/example/myapplication/MyApp.kt
+++ lab_02/app/src/main/java/com/example/myapplication/MyApp.kt
@@ -2,17 +2,40 @@ package com.example.myapplication
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.*
 import com.example.myapp.com.example.myapplication.ItemsListScreen
+import com.example.myapp.com.example.myapplication.MainScreen
 import com.example.myapplication.ItemDetailsScreen
 import com.example.myapplication.ui.theme.MyApplicationTheme
+
+//@Composable
+//fun MyApp() {
+//    MyApplicationTheme {
+//        // Создаем контроллер навигации
+//        val navController = rememberNavController()
+//
+//        // Настраиваем граф навигации
+//        NavHost(navController, startDestination = "items_list") {
+//            composable("items_list") {
+//                ItemsListScreen(navController = navController)
+//            }
+//            composable("item_details/{itemId}") { backStackEntry ->
+//                val itemId = backStackEntry.arguments?.getString("itemId")?.toIntOrNull()
+//                itemId?.let {
+//                    ItemDetailsScreen(itemId = it, navController = navController)
+//                }
+//            }
+//        }
+//    }
+//}
 
 @Composable
 fun MyApp() {
     MyApplicationTheme {
-        // Создаем контроллер навигации
         val navController = rememberNavController()
 
-        // Настраиваем граф навигации
-        NavHost(navController, startDestination = "items_list") {
+        NavHost(navController, startDestination = "main_screen") {
+            composable("main_screen") {
+                MainScreen()
+            }
             composable("items_list") {
                 ItemsListScreen(navController = navController)
             }
@@ -25,3 +48,4 @@ fun MyApp() {
         }
     }
 }
+
